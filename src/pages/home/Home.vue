e30028bb8cdbca398dec873000d99e0e8fb01ea0
aigou <2455835289@qq.com>
2018-05-18 17:25:06 +0800

    little change

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -4,6 +4,7 @@
         <home-swiper></home-swiper>
         <home-icons></home-icons>
         <home-commend></home-commend>
+        <home-weekend></home-weekend>
     </div>
 </template>
 
@@ -12,13 +13,14 @@ import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
 import HomeCommend from './components/Commend'
+import HomeWeekend from './components/Weekend'
 export default {
   name: 'Home',
   components:{
     HomeHeader,
     HomeSwiper,
     HomeIcons,
-    HomeCommend
+    HomeCommend,
   }
 }
 </script>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div class="space"></div>
+    <div class="title">
+      <div class="title-love">
+        <img src="../../../../static/love.png" alt="爱心">
+      </div>
+      <div class="title-text">
+         周末去哪儿
+      </div>
+    </div>
+    <ul>
+      <li class="item border-bottom" >
+        <img class="item-img" src="" :alt="">
+        <div class="item-info">
+          <p class="item-title"></p>
+          <p class="item-desc"></p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  // data: function () {
+  //  return {
+  //     weekend: [
+  //       {
+  //         id: '001',
+  //         imgUrl: '',
+  //         title: '',
+  //         desc: ''
+  //       },
+  //       {
+  //         id: '002',
+  //         imgUrl: '',
+  //         title: '',
+  //         desc: ''
+  //       },
+  //       {
+  //         id: '003',
+  //         imgUrl: '',
+  //         title: '',
+  //         desc: ''
+  //       },
+  //       {
+  //         id: '004',
+  //         imgUrl: '',
+  //         title: '',
+  //         desc: ''
+  //       }
+  //     ]
+  //   }
+  // }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mininx.styl';
+  .space
+    height .5rem
+    background #eeeeee
+  .title
+    line-height 2rem
+    text-indent .2rem
+    .title-love
+      float left
+      img
+        position relative
+        top .2rem 
+        width 1rem
+</style>
