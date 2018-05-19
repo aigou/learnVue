90d53009609074b9c4bd35c1eac3da18fd51c519
aigou <2455835289@qq.com>
2018-05-19 21:39:25 +0800

    city

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="header">
+      <router-link to="/">
+        <div class="header-left">
+          <div class="iconfont back-icon">&#xe720;</div>
+        </div>
+      </router-link>
+      <div class="header-title">
+        选择城市
+      </div>
+    </div>
+    <div class="city-search">
+      <input class="search-input" type="text" placeholder="请输入城市名">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+  .header
+    position relative
+    line-height: 2.2rem
+    background: $bgColor
+    color: #fff
+    .header-left
+      position absolute
+      width: 2.2rem
+      float: left
+      .back-icon
+        color #fff 
+        text-align: center
+        font-size: 1.5rem
+    .header-title
+      margin 0 2rem
+      text-align center
+  .city-search
+    height 1.8rem
+    background-color $bgColor
+    padding .2rem .8rem
+    .search-input
+      box-sizing border-box
+      width 100%
+      height 1.8rem
+      padding 0 .3rem
+      line-height 1.8rem
+      font-size .9rem
+      text-align center
+      color #666
+      border-width 0
+      border-radius .2rem
+      &:focus
+        outline-color #fff
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,9 +7,11 @@
             <span class="iconfont">&#xe741;</span>
             请输入城市/游玩主题
         </div>
-        <div class="header-right">
+        <router-link to="City">
+          <div class="header-right">
             {{city}}<span class="iconfont">&#xe749;</span>
-        </div>
+          </div>
+        </router-link>
     </div>
 </template>
 
@@ -24,44 +26,33 @@ export default {
 
 <style lang="stylus" scoped>
 @import '~styles/varibles.styl';
-
-.header {
-    display: flex;
-    line-height: 2.2rem;
-    background: $bgColor;
-    color: #fff;
-
-    .header-left {
-        width: 2.2rem;
-        float: left;
-
-        .back-icon {
-            text-align: center;
-            font-size: 1.5rem;
-        }
-    }
-
-    .header-input {
-        flex: 1;
-        margin-top: 0.3rem;
-        margin-right: 0.4rem;
-        height: 1.5rem;
-        line-height: 1.5rem;
-        padding-left: 0.4rem;
-        background: #fff;
-        border-radius: 0.3rem;
-        color: #ccc;
-    }
-
-    .header-right {
-        width: 3rem;
-        float: right;
-        margin-right: 0.4rem;
-        text-align: center;
-
-        span {
-            font-size: 1rem;
-        }
-    }
-}
+.header
+  display: flex
+  line-height: 2.2rem
+  background: $bgColor
+  color: #fff
+  .header-left
+    width: 2.2rem
+    float: left
+    .back-icon
+      text-align: center
+      font-size: 1.5rem
+  .header-input
+    flex: 1
+    margin-top: 0.3rem
+    margin-right: 0.4rem
+    height: 1.5rem
+    line-height: 1.5rem
+    padding-left: 0.4rem
+    background: #fff
+    border-radius: 0.3rem
+    color: #ccc
+  .header-right 
+    width: 3rem
+    float: right
+    margin-right: 0.4rem
+    text-align: center
+    color #fff
+    span
+      font-size: 1rem;
 </style>
