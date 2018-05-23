9d8007eaa4a62007ff02e7732fe81b035461a310
aigou <2455835289@qq.com>
2018-05-23 20:30:38 +0800

    city-search

--- src/pages/city/components/Header.vue
+++ src/pages/city/components/Header.vue
@@ -11,14 +11,68 @@
       </div>
     </div>
     <div class="city-search">
-      <input class="search-input" type="text" placeholder="请输入城市名">
+      <input v-model="keyword" class="search-input" type="text" placeholder="请输入城市名">
+    </div>
+    <div class="search-content" ref="search" v-show="keyword">
+      <ul>
+        <li v-for="item of list" class="search-list" v-text="item.name" :key="item.id"></li>
+        <li class="search-list" v-show="noData">没有匹配的城市</li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'CityHeader'
+  name: 'CityHeader',
+  props: {
+    cities: Object
+  },
+  data: function () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null,
+      nolist: false
+    }
+  },
+  computed: {
+    noData: {
+      get: function () {
+        return this.nolist
+      },
+      set: function () {
+        if (this.list.length === 0) {
+          this.nolist = true
+        } else {
+          this.nolist = false
+        }
+        return this.nolist
+      }
+    }
+  },
+  watch: {
+    keyword: function () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      this.timer = setTimeout(() => {
+        const res = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              res.push(value)
+            }
+          })
+        }
+        this.list = res
+      }, 100)
+    }
+  },
+  mounted: function () {
+    this.scroll = new Bscroll(this.$refs.search)
+  }
 }
 </script>
 
@@ -57,4 +111,20 @@ export default {
       border-radius .2rem
       &:focus
         outline-color #fff
+  .search-content
+    z-index 1
+    overflow hidden
+    position absolute
+    top 4.3rem
+    left 0
+    right 0
+    bottom 0
+    background-color #eee
+    .search-list
+      line-height 1.6rem
+      font-size .9rem
+      color #555
+      padding-left 1rem
+      background-color #fff
+      border-bottom .05rem solid #ddd
 </style>
